37bea8679a872a29e641b215b138d332f106b4ef
Malcolm Hauser <137590820+Mooshay105@users.noreply.github.com>
2025-12-09 11:11:25 +1000

    Patching CVE-2025-55182, Yay! Add a redirect to `https://malcolmjh.com`

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import ReactDOM from "react-dom/client";
 import logo from "./assets/img/logo.png";
 import "./assets/main.css";
+import { useEffect, useState } from "react";
 
 function App() {
-	return (
+	const [secondsToRedirect, setSecondsToRedirect] = useState(10)
+
+	useEffect(() => {
+		if (secondsToRedirect == 0) {
+			window.location.href = 'https://www.malcolmjh.com/';
+		} else {
+			setSecondsToRedirect(secondsToRedirect-1)
+		}
+	}, [secondsToRedirect])
+
+ 	return (
 		<div className="main">
 			<img src={logo} alt="Kiwi Micro Logo" width="200" height="200" />
-			<h1>New Site Comming Soon!</h1>
+			<h1>New Site Comming Soon! (Maybe)</h1>
+			<p>Redirecting you to my personal site in {secondsToRedirect}</p>
+      		<p>If you're not redirected, <a href="https://www.malcolmjh.com/">click here</a>.</p>
 		</div>
-	);
+  	);
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
